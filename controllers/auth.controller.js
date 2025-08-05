a4fff8207f23183ee5baae9f131e0fa3f5b5dfbd
Harsh Saini <harshsaini555333@gmail.com>
2025-08-05 11:34:49 +0530

    auth

---
+++ controllers/auth.controller.js
@@ -0,0 +1,70 @@
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const generateToken = require("../utils/generateToken");
+
+exports.signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const user = new User({ username, email, password });
+    if (!username) {
+      return res.status(400).json({ message: "Username is required" });
+    }
+    if (!email) {
+      return res.status(400).json({ message: "Email is required" });
+    }
+    if (!password) {
+      return res.status(400).json({ message: "Password is required" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user.password = hashedPassword;
+    await user.save();
+
+    const token = generateToken(user)
+    res.status(201).json({
+      message: "User created & logged in",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+      token,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = new User({ email, password });
+    if (!email) return res.status(400).json({ message: "Email is required" })
+    if (!password) return res.status(400).json({ message: "Password is required" })
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) return res.status(400).json({ message: "User does not exist" })
+    const isMatch = await bcrypt.compare(password, existingUser.password)
+    if(!isMatch) return res.status(400).json({message: "Invalid Password"})
+
+    const token = generateToken(existingUser)
+    res.status(201).json({
+      message: "User Logged In Successfully",
+      user: {
+        id: existingUser._id,
+        username: existingUser.username,
+        email: existingUser.email,
+      },
+      token,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const connectDB = require('./config/db')
+const dotenv = require('dotenv')
+dotenv.config()
+const app = express()
+app.use(express.json())
+connectDB()
+
+app.get('/', (req,res)=>{
+    res.send('Admin Homepage')
+})
+
+app.use('/admin/user', require('./routes/auth.routes'))   //signup&login routes
+// app.use('/admin/user', require('./routes/user.routes'))   //protected routes
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
